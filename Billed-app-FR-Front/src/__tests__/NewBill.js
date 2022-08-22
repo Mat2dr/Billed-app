60722e69ee2e421a0b82e6d9d33cd2e49a817f27
Mathias Dragovic <dragovic.mathias@gmail.com>
2022-08-22 18:58:38 +0200

    NewBill Test: if file is correct

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -2,32 +2,39 @@
  * @jest-environment jsdom
  */
 
+//Import testing modules
 import { screen, waitFor } from "@testing-library/dom";
+import userEvent from '@testing-library/user-event';
+//Import storage module
+import store from "../__mocks__/store.js";
+//Import routes and router modules
+import Router from "../app/Router.js";
+import { ROUTES, ROUTES_PATH } from "../constants/routes.js";
+//Import dashboards
 import BillsUI from "../views/BillsUI.js";
 import NewBillUI from "../views/NewBillUI.js";
 import NewBill from "../containers/NewBill.js";
-import { ROUTES, ROUTES_PATH } from "../constants/routes.js";
-import {localStorageMock} from "../__mocks__/localStorage.js";
-import userEvent from '@testing-library/user-event';
-import mockStore from "../__mocks__/store";
-import router from "../app/Router.js";
+
 
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
     beforeEach(() => {
-      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
-      window.localStorage.setItem('user', JSON.stringify({
-        type: 'Employee'
-      }))
-      document.body.innerHTML = NewBillUI();
-      const onNavigate = (pathname) => {
-        document.body.innerHTML = ROUTES({ pathname })
-      }
-      const store = null;
-      const newBillDash = new NewBill({
-        document, onNavigate, store, localStorage: window.localStorage
+      const user = JSON.stringify({
+        type: "Employee",
+        email: "a@a",
       });
+      window.localStorage.setItem("user", user);
+
+      const pathname = ROUTES_PATH["NewBill"];
+      Object.defineProperty(window, "location", {
+        value: {
+          hash: pathname,
+        },
+      });
+
+      document.body.innerHTML = `<div id="root"></div>`;
+      Router();
      });
 
     test("Page loaded", () => {
@@ -35,41 +42,35 @@ describe("Given I am connected as an employee", () => {
       expect(title).toBeTruthy();
     })
 
-    test("Then mail icon in vertical layout should be highlighted", async () => {
-      await waitFor(() => screen.getByTestId('icon-mail'))
-
-      const mailIcon = screen.getByTestId('icon-mail')
-      expect(mailIcon.classList.contains('active-icon')).toBe(true)
+    test("Then mail icon in vertical layout should be highlighted", () => {
+      expect(screen.getByTestId('icon-mail').classList.contains('active-icon')).toBe(true)
     })
+  })
 
-    describe("When I want to add a file", () => {
-      test("The file is correct", () => {
-        Object.defineProperty(window, 'localStorage', { value: localStorageMock })
-        window.localStorage.setItem('user', JSON.stringify({
-          type: 'Employee'
-        }))
-
-        const onNavigate = (pathname) => {
-          document.body.innerHTML = ROUTES({ pathname })
-        }
-        document.body.innerHTML = NewBillUI();
-        const store = null;
+  describe("When I want to add a file", () => {
+    test("The file is correct", async () => {
+      //Set the test
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      };
+      const newBillDash = new NewBill({
+        document,
+        onNavigate,
+        store,
+        localStorage: window.localStorage,
+      });
       
-        const newBillDash = new NewBill({
-          document, onNavigate, store, localStorage: window.localStorage
-        });
+      //Testing
+      const handleChangeFile = jest.fn(newBillDash.handleChangeFile);
+      const addFileBtn = screen.getByTestId("file");
+      addFileBtn.addEventListener("change", handleChangeFile);
+      //Upload the file called file
+      const file = new File(['hello.jpg'], 'hello.jpg', {type: 'image/jpeg'});
+      userEvent.upload(addFileBtn, file);
       
-        const handleChangeFile = jest.fn(newBillDash.handleChangeFile);
-        const addFileBtn = screen.getByTestId("file");
-        addFileBtn.addEventListener("change", handleChangeFile);
-      
-        const file = new File(['hello.jpg'], 'hello.jpg', {type: 'image/jpeg'});
-        userEvent.upload(addFileBtn, file);
-      
-        //expect(handleChangeFile).toHaveBeenCalled();
-        expect(addFileBtn.files.length).toEqual(1);
-      })
+      //Check to pass
+      expect(handleChangeFile).toHaveBeenCalled();
+      expect(addFileBtn.files.length).toEqual(1);
     })
-
   })
 })
