a2f8e877ba2539eb953e400bc401004401a213d6
Mathias Dragovic <dragovic.mathias@gmail.com>
2022-09-07 17:27:34 +0200

    All test working

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -3,10 +3,13 @@
  */
 
 //Import testing modules
-import { screen, waitFor } from "@testing-library/dom";
+import '@testing-library/jest-dom';
+import { getByRole, getByTestId, getByLabelText } from '@testing-library/dom'
+import { fireEvent,screen, waitFor } from "@testing-library/dom";
 import userEvent from '@testing-library/user-event';
 //Import storage module
 import store from "../__mocks__/store.js";
+import {localStorageMock} from "../__mocks__/localStorage.js";
 //Import routes and router modules
 import Router from "../app/Router.js";
 import { ROUTES, ROUTES_PATH } from "../constants/routes.js";
@@ -72,5 +75,110 @@ describe("Given I am connected as an employee", () => {
       expect(handleChangeFile).toHaveBeenCalled();
       expect(addFileBtn.files.length).toEqual(1);
     })
+    test("file is correct and I click on the btn envoyer", () => {
+      const mockedFormEvent = {
+        target: { querySelector: jest.fn() },
+        preventDefault: jest.fn(),
+      };
+      mockedFormEvent.target.querySelector.mockReturnValue("valeur de test");
+  
+      document.body.innerHTML = NewBillUI();
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      };
+      const newBillDash = new NewBill({
+        document,
+        onNavigate,
+        store,
+        localStorage: window.localStorage,
+      });
+  
+      const handleSubmit = jest.fn(() =>
+        newBillDash.handleSubmit(mockedFormEvent)
+      );
+  
+      //Test handleSubmit
+      const envoyerBtn = screen.getByTestId("envoyer-btn");
+      envoyerBtn.addEventListener("click", handleSubmit);
+      userEvent.click(envoyerBtn);
+      expect(handleSubmit).toHaveBeenCalled();
+    });
+  })
+})
+
+// test d'intégration Post
+describe("Given I am a user connected as Employee", () => {
+  describe("When I navigate to NewBillDash", () => {
+    test("POST", async () => {
+      //Set the test
+      document.body.innerHTML = NewBillUI();
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      };
+      const newBillDash = new NewBill({
+        document,
+        onNavigate,
+        store,
+        localStorage: window.localStorage,
+      });
+      const billTest = {
+        id: "10000",
+        status: "pending",
+        pct: 30,
+        amount: 500,
+        email: "dragovic.mathias@gmail.com",
+        name: "Le remois",
+        vat: "15",
+        fileName: "test.jpg",
+        date: "2022-09-10",
+        commentary: "postMockNewBill",
+        type: "Restaurants et bars",
+        fileUrl: "https://test.jpg",
+      };
+
+      const handleSubmit = jest.fn(newBillDash.handleSubmit);
+      const formNewBills = screen.getByTestId('form-new-bill')
+			formNewBills.addEventListener('submit', handleSubmit)
+			fireEvent.submit(formNewBills)
+			expect(handleSubmit).toHaveBeenCalled()
+    })
+   describe("When an error occurs on API", () => {
+    //Set the test
+    beforeEach(() => {
+      jest.spyOn(store, "bills")
+      Object.defineProperty(
+          window,
+          'localStorage',
+          { value: localStorageMock }
+      )
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee',
+        email: "e@e"
+      }))
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.appendChild(root)
+      Router()
+    })
+    test("fetches bills from an API and fails with 404 message error", () => {
+      store.bills.mockImplementationOnce(() => {
+        return Promise.reject(new Error("Erreur 404"))
+      })
+      document.body.innerHTML =  BillsUI({ error: "Erreur 404" });
+      const message = screen.getByText(/Erreur 404/)
+      expect(message).toBeTruthy()
+    })
+
+    test("fetches messages from an API and fails with 500 message error", () => {
+
+      store.bills.mockImplementationOnce(() => {
+        return Promise.reject(new Error("Erreur 500"))
+      })
+      document.body.innerHTML =  BillsUI({ error: "Erreur 500" });
+      const message = screen.getByText(/Erreur 500/)
+      expect(message).toBeTruthy()
+    })
+  })
+
   })
 })
--- Billed-app-FR-Front/src/views/NewBillUI.js
+++ Billed-app-FR-Front/src/views/NewBillUI.js
@@ -27,7 +27,7 @@ export default () => {
                   </div>
                   <div class="col-half">
                     <label for="expense-name" class="bold-label">Nom de la dépense</label>
-                    <input type="text" class="form-control blue-border" data-testid="expense-name" placeholder="Vol Paris Londres" />
+                    <input type="text" id="expense-name" name="expense-name" class="form-control blue-border" data-testid="expense-name" placeholder="Vol Paris Londres" />
                   </div>
                   <div class="col-half">
                     <label for="datepicker" class="bold-label">Date</label>
@@ -62,7 +62,7 @@ export default () => {
             <div class="row">
               <div class="col-md-6">
                 <div class="col-half">
-                  <button type="submit" id='btn-send-bill' class="btn btn-primary">Envoyer</button>
+                  <button type="submit" data-testid="envoyer-btn" id='btn-send-bill' class="btn btn-primary">Envoyer</button>
                 </div>
               </div>
             </div>
